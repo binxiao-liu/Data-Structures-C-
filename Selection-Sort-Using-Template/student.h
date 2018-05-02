1c2919f5ba0ba7f5bdd55bd78171d399a5415274
binxiao-liu <1260115534@qq.com>
2018-05-02 22:45:08 +0800

    选择排序法  使用模板（泛型）编写算法  随机生成算法测试用例

---
+++ Selection-Sort-Using-Template/student.h
@@ -0,0 +1,27 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct Student{
+    string name;
+    int score;
+
+    // 重载小于运算法,定义Student之间的比较方式
+    // 如果分数相等，则按照名字的字母序排序
+    // 如果分数不等，则分数高的靠前
+    bool operator<(const Student &otherStudent){
+        return score != otherStudent.score ?
+                    score > otherStudent.score : name < otherStudent.name;
+    }
+
+    friend ostream& operator<<(ostream &os,const Student &student){
+        os<<"Student:"<<student.name<<" "<<student.score<<endl;
+        return os;
+    }
+};
+
+#endif // STUDENT_H
---
+++ SelecttionSort/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include <algorithm>
+#include"sorttesthelper.h"
+using namespace std;
+template<typename T>
+void selectionSort(int arr[], int n){
+
+    for(int i = 0 ; i < n ; i ++){
+        // 寻找[i, n)区间里的最小值
+        int minIndex = i;
+        for( int j = i + 1 ; j < n ; j ++ )
+            if( arr[j] < arr[minIndex] )
+                minIndex = j;
+
+        swap( arr[i] , arr[minIndex] );
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+//    int a[10] = {10,9,8,7,6,5,4,3,2,1};
+//    selectionSort(a,10);
+//    for( int i = 0 ; i < 10 ; i ++ )
+//         cout<<a[i]<<" ";
+//    cout<<endl;
+    int n=10000;
+    int *arr = SortTestHelper::generateRandomArray(n,0,n);
+    selectionSort(arr,n);
+//    for( int i = 0 ; i < n ; i ++ )
+//          cout<<arr[i]<<" ";
+//     cout<<endl;
+    SortTestHelper::printArray(arr,n);
+    delete[] arr;
+    return 0;
+}
